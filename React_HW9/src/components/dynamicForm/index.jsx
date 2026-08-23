9a25176215ac37a1a5b292e61fc89e561e4996d6
Dzintars <d.krakops@t-online.de>
2026-08-23 19:56:23 +0200

    Clear project , first steps, funcion

---
+++ React_HW9/src/components/dynamicForm/index.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import styles from './styles.module.css';
+
+function DynamicForm() {
+  return (
+    <div className={styles.formContainer}>
+      <h2>Динамическая форма</h2>
+      {/* Здесь на следующих шагах мы добавим поля ввода и логику проверки */}
+    </div>
+  );
+}
+
+export default DynamicForm;
